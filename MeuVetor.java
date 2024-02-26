3c0837eee5f7f34e5faa1066b2afcf2b269b70b8
YASMIN PACHECO GIL BONILHA <23.01425-3@maua.br>
2024-02-26 09:18:42 -0300

    terceira aula - redimensionamento

--- MeuVetor.java
+++ MeuVetor.java
@@ -36,18 +36,56 @@ public class MeuVetor {
             // apesar disso, o else não seria necessário, já que, se o if for verdadeiro, ele vai sair do laço e não chegará até o falso. além disso, podemos simplesmente retornar o próprio teste, como : return ultimaPos == v.length -1;
     }
 
-    public boolean add(int elemento){
-        if(estaCheio())
-            return false;
-        ultimaPos++;
-        v[ultimaPos] = elemento;
-        return true;
-    }
+    // public boolean add(int elemento){ neste caso, o vetor está fixo - nós escolhemos esse tamanho e ele fica desse tamanho para sempre
+    //     if(estaCheio())
+    //         return false;
+    //     ultimaPos++;
+    //     v[ultimaPos] = elemento;
+    //     return true;
 
+    //     // por que não colocamos um print aqui para vermos se deu certo? pelo padrão MVC, onde não misturamos modelos (regras de negócio ) com visão (interface), já que não sabemos se o cliente que vai usar nosso programa vai ser um usuário final ou um outro software, que talvez não tenha uma saída visual e, portanto, não consegue fazer o tratamento desse print. já o vetor é totalmente tratável - o print não. 
+    // }
+
+    // análise amortizada do custo computacional (pesquisar!!!!) - se encher, a gente dobra. se estiver só um quarto, a genre reduz pela metade.
+
+    // não precisamos mais do retorno boolean, só se for em c por exemplo ou em tempo de execução, como em dispositivos com menos memória
+    public void add(int elemento) {
+        if(estaCheio()){
+           redimensiona(v.length*2);
+        }
+        v[++ultimaPos] = elemento; //primeiro soma depois atribui
+    }
+    
+    
+    // na classe array list, os argumentos sao da classe generics, que so recebe classes, porque assim podemos conferir se aquela posicao realmente tera algo ou ela esta nula
+    // quando queremos dar um clear, não temos que apagar tudo, só temos que colocar o ponteiro no começo, porque dai ele vai comecar a sobrescrever em cima das informações (elas estão "apagadas" pois não tem mais referencia na memoria)
+    
     public boolean estaVazio(){
         return ultimaPos == -1;
     }
+    
+    public int remove(){
+        if(estaVazio()) return 0;
+        int aux = v[ultimaPos];
+        ultimaPos--;
+        if(v.length >= 10 && ultimaPos <= v.length/4) { //para que, se por acaso der menos, ele nao quebre
+            redimensiona(v.length/2);
+        }
+        return aux;
+    }
+    
+    
+    private void redimensiona(int novaCapacidade){
+        int[] temp = new int [novaCapacidade]; //esse vetor está dissociado na memória (sem referência), é um vetor temporário. como vamos cortar a referência do vetor original e attach it aqui, vamos deixar o outro vetor sem referencia e o garbagge (JVM) vai cuidar de liberar ele da memoria
+        // se estivermos em uma linguagem que não tem essa propriedade, teríamos que carregar a "capacidade" como atributo e passar ela
 
+        for(int i=0; i<ultimaPos; i++){
+            temp[i] = v[i];
+        }
+        v = temp; //atribui a ref do novo vetor ao atributo v;
+        //cortamos os laços e passamos os valores de temp para v (como temp é uma variável local de metodo, ela morre quando o metodo termina)
+    }
+    
     @Override // aviso para o desenvolvedor de que não podemos mexer na assinatura do método, já que queremos que haja sobrescrita
     public String toString(){
         String s = "";
--- Teste1.java
+++ Teste1.java
@@ -1,19 +1,31 @@
 public class Teste1 {
     public static void main(String[] args) {
         MeuVetor v1 = new MeuVetor(10);
-        System.out.println("última posição depois de instanciar: " + v1.getUltimaPos());
+        System.out.println("ultima posicao depois de instanciar: " + v1.getUltimaPos());
         // v1.ultimaPos = 27;
         // v1.v[v1.ultimaPos] =10;
-        v1.setUltimaPos(27);
-        System.out.println("ultima posicao depois do 27: " + v1.getUltimaPos());
-        System.out.println(v1);
+        // v1.setUltimaPos(27);
+        // System.out.println("ultima posicao depois do 27: " + v1.getUltimaPos());
+        // System.out.println(v1);
 
-        if(v1.add(12)){
-            System.out.println("elemento inserido com sucesso!");
-        }else{
-            System.out.println("o vetor está cheio!");
-        };
+        // if(v1.add(12)){
+        //     System.out.println("elemento inserido com sucesso!");
+        // }else{
+        //     System.out.println("o vetor está cheio!");
+        // };
+        
+        for(int n=0; n<100; n++){
+            v1.add(n);
 
+            System.out.println("capacidade atual para  " + n + ":   "+ v1.getV().length);
+        }
+
+        System.out.println("*****************");
+
+        while(!v1.estaVazio()){
+            v1.remove();
+            System.out.println("capacidade atual para  " + v1.getUltimaPos() + ":   " + v1.getV().length);
+        }
         
     }
 }
@@ -21,3 +33,5 @@ public class Teste1 {
 // quando printamos os vetores, o que aparece é o nomeClasse@hashCode, que dá uma chave de como você consegue acessar esse objeto;
 // temos o polimorfismo dinâmico (sobrecarga), estático (sobrescrita) e paramétrico (generics).
 // polimorfismo dinamico pq ele é resolvido em tempo de execucao. estatico pq e resolvido em tempo de compilacao. e o parametrico é o que define a partir do que a gente definiu
+
+// quando chamamos o metodo getV, temos como retorno para nos a referencia na memoria do vetor V
